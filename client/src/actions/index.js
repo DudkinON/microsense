4ba10f80d5ca37a0b937e8bbebae53710a00aa99
Oleg Dudkin <dudkin.on@gmail.com>
2019-07-21 20:51:26 -0700

    feat: Update actions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -12,3 +12,23 @@ export const getReaders = url => dispatch => get(url, types.SET_READERS, dispatc
 export const getHealth = url => dispatch => get(url, types.SET_HEALTH, dispatch);
 
 export const getOperations = url => dispatch => get(url, types.SET_OPERATIONS, dispatch);
+
+export const setMessage = msg => dispatch => dispatch(
+  {type: types.SET_MESSAGE, payload: msg}
+);
+
+export const startJob = (url, data) => dispatch => http.post(url, data)
+  .then(res => {
+    if (res && res.status === 200) dispatch(setMessage({
+      type: 'success',
+      message: "Job was started!"
+    }));
+    else dispatch(setMessage({
+      type: 'error',
+      message: "Server is unavailable, try it later"
+    }));
+  })
+  .catch(e => dispatch(setMessage({
+    type: 'error',
+    message: "Server is unavailable, try it later"
+  })));

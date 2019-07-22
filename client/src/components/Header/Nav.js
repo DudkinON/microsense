a6ac34683625de083fa631ebfd9670821065fe98
Oleg Dudkin <dudkin.on@gmail.com>
2019-07-21 18:50:31 -0700

    refactor: Nav component to state less component

--- client/src/components/Header/Nav.js
+++ client/src/components/Header/Nav.js
@@ -1,36 +1,26 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import {Link} from 'react-router-dom';
 
-function mapStateToProps(state) {
-  return {};
-}
 
-export class Nav extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">Navbar</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
+export const Nav = () => (
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <a className="navbar-brand" href="#">MicroSense™</a>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"/>
+    </button>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item active">
-              <Link className="nav-link" to="#">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="#">Link</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    );
-  }
-}
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item active">
+          <Link className="nav-link" to="#">Home</Link>
+        </li>
+        <li className="nav-item disabled">
+          <Link className="nav-link" to="#">Account</Link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+);
 
-export default connect(
-  mapStateToProps,
-)(Nav);
+export default Nav;
